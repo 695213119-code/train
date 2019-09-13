853fa4b517a65bfe268a649157ef7458662b0b9f
zhanglei <18827529732@163.com>
2019-09-13 15:48:20 +0800

    提交train的注册中心以及网关服务

---
+++ parent-service/gateway-service/src/main/java/com/train/gatewayservice/filter/GatewayFilter.java
@@ -0,0 +1,41 @@
+package com.train.gatewayservice.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+
+/**
+ * 网关拦截器
+ *
+ * @author zhangLei
+ * @serial 2019/09/13
+ */
+public class GatewayFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return null;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return false;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        //此方法为网关拦截的具体实现方法
+
+        //TODO 校验接口是否需要验证token
+
+        //TODO 验证token是否合法
+
+        //TODO 用户鉴权
+
+        return null;
+    }
+}
